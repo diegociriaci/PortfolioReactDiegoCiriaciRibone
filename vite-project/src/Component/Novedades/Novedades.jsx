c76bb54b50a5e7744a684923b7721bdccdacfe56
diegociriaciribone <diegociriaci@hotmail.com>
2024-07-24 20:31:28 -0300

    Modifique el jsx y el css del componente Novedades

--- vite-project/src/Component/Novedades/Novedades.jsx
+++ vite-project/src/Component/Novedades/Novedades.jsx
@@ -3,46 +3,41 @@ import './Novedades.css'
 const Novedades = () => {
     return(
 	    <section className="novedades py-5">
-            <div className="container">
-                <div className="row">
-                    <div className="novedades-header col-12 mb-4">
-                        <h1>Novedades</h1>
-                    </div>
+    <div className="container">
+        <div className="row">
+            <div className="novedades-header col-12 mb-4">
+                <h1>Novedades</h1>
+            </div>
+        </div>
+        <div className="card-container">
+            <div className="card h-100 card-custom">
+                <div className="imagen-novedad">
+                    <img src="img/img-novedad-1.jpg" className="card-img-top" alt="Novedad 1" />
                 </div>
-                <div className="row justify-content-center">
-                    <div className="col-md-4 mb-4">
-                        <div className="card h-100 card-custom">
-                            <div className="imagen-novedad">
-                                <img src="img/img-novedad-1.jpg" className="card-img-top" alt="Novedad 1" />
-                            </div>
-                            <div className="card-body">
-                                <p className="card-text">Nuevas tendencias en experiencia de usuario: La importancia de la personalización</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-4">
-                        <div className="card h-100 card-custom">
-                            <div className="imagen-novedad">
-                                <img src="img/img-novedad-2.jpg" className="card-img-top" alt="Novedad 2" />
-                            </div>
-                            <div className="card-body">
-                                <p className="card-text">COVID-19: Impacto en el diseño de experiencia de usuario</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-4">
-                        <div className="card h-100 card-custom">
-                            <div className="imagen-novedad">
-                                <img src="img/img-novedad-3.jpg" className="card-img-top" alt="Novedad 3" />
-                            </div>
-                            <div className="card-body">
-                                <p className="card-text">Descubriendo los detalles: Entrevista con un experto en UX/UI</p>
-                            </div>
-                        </div>
-                    </div>
+                <div className="card-body">
+                    <p className="card-text">Nuevas tendencias en experiencia de usuario: La importancia de la personalización</p>
                 </div>
             </div>
-        </section>
+            <div className="card h-100 card-custom">
+                <div className="imagen-novedad">
+                    <img src="img/img-novedad-2.jpg" className="card-img-top" alt="Novedad 2" />
+                </div>
+                <div className="card-body">
+                    <p className="card-text">COVID-19: Impacto en el diseño de experiencia de usuario</p>
+                </div>
+            </div>
+            <div className="card h-100 card-custom">
+                <div className="imagen-novedad">
+                    <img src="img/img-novedad-3.jpg" className="card-img-top" alt="Novedad 3" />
+                </div>
+                <div className="card-body">
+                    <p className="card-text">Descubriendo los detalles: Entrevista con un experto en UX/UI</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
     )
 }
 
