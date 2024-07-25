c9ef7f46f950698de6e5dea42e6beac69e81ecf3
diegociriaciribone <diegociriaci@hotmail.com>
2024-07-25 20:03:09 -0300

    Hice que el menu se moviera mientras uno va scroleando por la pagina

--- vite-project/src/Component/Header/Header.jsx
+++ vite-project/src/Component/Header/Header.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+    const [scrolling, setScrolling] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setScrolling(true);
+        } else {
+            setScrolling(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [])
+
     return (
-        // <header className="bg-white py-5 d-flex align-items-center header-custom">
-        //     <nav className="nav-container d-flex justify-content-center flex-grow-1">
-        //         <a className="nav-link" href="#">HOME</a>
-        //         <a className="nav-link" href="#">ACERCA DE MI</a>
-        //         <a className="nav-link" href="#">SERVICIOS</a>
-        //         <a className="nav-link" href="#">HABILIDADES Y EXPERIENCIA</a>
-        //         <a className="nav-link" href="#">ULTIMOS TRABAJOS</a>
-        //         <a className="nav-link" href="#">OPINIONES DE CLIENTES</a>
-        //         <a className="nav-link" href="#">NOVEDADES</a>               
-        //     </nav>
-        //     <a className="nav-link hablemos" href="#">Hablemos!</a>
-        // </header>
-    <header className="bg-white py-4 d-flex align-items-center header-custom">
-        <div className="container-fluid">
+    <header className={`bg-white py-4 d-flex align-items-center header-custom sticky-top ${scrolling ? 'header-transparent' : 'header-solid'}`}>        
+        <div className="container-fluid ">
             <nav className="navbar navbar-expand-lg">
-                {/* <a className="navbar-brand" href="#">Navbar</a> */}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
