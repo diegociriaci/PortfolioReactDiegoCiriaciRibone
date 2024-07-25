a3c31ff60826d6d5943c98b98fcd16c8a07d12ea
diegociriaciribone <diegociriaci@hotmail.com>
2024-07-25 11:54:45 -0300

    Modifique el componente Header cambiando todas las medidas de px a rem y modificando los link del menu

--- vite-project/src/Component/Header/Header.jsx
+++ vite-project/src/Component/Header/Header.jsx
@@ -6,9 +6,12 @@ const Header = () => {
         <header className="header bg-white py-5">
             <nav className="nav-container d-flex justify-content-center">
                 <a className="nav-link" href="#">HOME</a>
-                <a className="nav-link" href="#">PORTFOLIO</a>
-                <a className="nav-link" href="#">ABOUT</a>
-                <a className="nav-link" href="#">PAGE</a>
+                <a className="nav-link" href="#">ACERCA DE MI</a>
+                <a className="nav-link" href="#">SERVICIOS</a>
+                <a className="nav-link" href="#">HABILIDADES Y EXPERIENCIA</a>
+                <a className="nav-link" href="#">ULTIMOS TRABAJOS</a>
+                <a className="nav-link" href="#">OPINIONES DE CLIENTES</a>
+                <a className="nav-link" href="#">NOVEDADES</a>               
             </nav>
             <a className="nav-link hablemos" href="#">Hablemos!</a>
         </header>
