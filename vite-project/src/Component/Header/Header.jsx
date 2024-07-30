32465f196c1413e682aae4824f18548088e477b1
diegociriaciribone <diegociriaci@hotmail.com>
2024-07-29 21:43:23 -0300

    Modifique la funcion de scroll del menu y puli detalles de la navegacion de la pagina con el navbar agregando offset a cada link

--- vite-project/src/Component/Header/Header.jsx
+++ vite-project/src/Component/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-scroll'
+import throttle from 'lodash.throttle'
 import './Header.css'
 
 const Header = ({ onShowModal }) => {
     const [scrolling, setScrolling] = useState(false)
 
-    const handleScroll = () => {
-        if (window.scrollY > 50) {
-            setScrolling(true)
-        } else {
-            setScrolling(false)
+    const handleScroll = throttle(() => {
+        if (window.scrollY > 50 && !scrolling) {
+            setScrolling(true);
+        } else if (window.scrollY <= 50 && scrolling) {
+            setScrolling(false);
         }
-    }
+    }, 200)
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll)
@@ -19,7 +20,7 @@ const Header = ({ onShowModal }) => {
         return () => {
             window.removeEventListener('scroll', handleScroll)
         }
-    }, [])
+    }, [scrolling])
 
     return (
         <header className={`bg-white py-4 d-flex align-items-center header-custom sticky-top ${scrolling ? 'header-transparent' : 'header-solid'}`}>        
@@ -30,13 +31,14 @@ const Header = ({ onShowModal }) => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav d-flex justify-content-center flex-grow-1">
-                            <a className="nav-link" href="#">HOME</a>
-                            <Link className="nav-link" to="acerca-de-mi" smooth={true} duration={50}>ACERCA DE MI</Link>
-                            <Link className="nav-link" to="servicios" smooth={true} duration={50}>SERVICIOS</Link>
-                            <Link className="nav-link" to="skills" smooth={true} duration={50}>HABILIDADES Y EXPERIENCIA</Link>
-                            <Link className="nav-link" to="trabajos" smooth={true} duration={50}>ULTIMOS TRABAJOS</Link>
-                            <Link className="nav-link" to="reviews" smooth={true} duration={50}>OPINIONES DE CLIENTES</Link>
-                            <Link className="nav-link" to="novedades" smooth={true} duration={50}>NOVEDADES</Link>
+                            {/* <a className="nav-link" href="#">HOME</a> */}
+                            <a className="nav-link" href="#" onClick={() => scroll.scrollToTop()}>HOME</a>
+                            <Link className="nav-link" to="acerca-de-mi" smooth={true} duration={50} offset={-47}>ACERCA DE MI</Link>
+                            <Link className="nav-link" to="servicios" smooth={true} duration={50} offset={-47}>SERVICIOS</Link>
+                            <Link className="nav-link" to="skills" smooth={true} duration={50} offset={-47}>HABILIDADES Y EXPERIENCIA</Link>
+                            <Link className="nav-link" to="trabajos" smooth={true} duration={50} offset={-47}>ULTIMOS TRABAJOS</Link>
+                            <Link className="nav-link" to="reviews" smooth={true} duration={50} offset={-47}>OPINIONES DE CLIENTES</Link>
+                            <Link className="nav-link" to="novedades" smooth={true} duration={50} offset={-47}>NOVEDADES</Link>
                             <a className="nav-link hablemos" href="#" onClick={onShowModal}>Hablemos!</a>
                         </div>
                     </div>
